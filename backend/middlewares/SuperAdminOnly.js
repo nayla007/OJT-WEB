cdf6433b3673c4e92e8b94dee1e5e87e702b4358
nayla <naylaameliaerista@gmail.com>
2025-11-11 20:00:32 +0700

    upload frontend

--- backend/middlewares/SuperAdminOnly.js
+++
@@ -1,6 +0,0 @@
-export default function superAdminOnly(req, res, next) {
-    if (req.user.role !== 'superadmin') {
-        return res.status(403).json({ message: 'Hanya superadmin yang bisa mengakses'});
-    }
-    next();
-}
